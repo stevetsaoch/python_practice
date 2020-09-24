9d047624f1af21c6611f4bfb7605599371696ba3
Steve <stevetsaoch@gmail.com>
2020-09-24 16:08:11 +0800

    while

--- Practice.py
+++ Practice.py
@@ -1,16 +1,38 @@
-#practice for loop
+##practice for loop
 
-for item in [1,2,3,4,5]:
-    print(item)
+# for item in [1,2,3,4,5]:
+#     print(item)
 
-user = {
-    'name' :'Steve',
-    'age' : 10,
-    'Height' : 100
-}
+# user = {
+#     'name' :'Steve',
+#     'age' : 10,
+#     'Height' : 100
+# }
 
-for key, value in user.items():
-    print(key, value)
+# for key, value in user.items():
+#     print(key, value)
 
-for item in user.values():
-    print(item)
+# for item in user.values():
+#     print(item)
+
+##counter
+# sumup = [1,2,3,4,5,6,7,8,9,10]
+# counter = 0
+# for num in sumup:
+#     counter = counter + num
+
+# print(counter)
+
+##range
+# for item in range(1,101,1):
+#     print(item)
+
+##enumerate
+# for i, char in enumerate(list(range(10))):
+#   if char == 5:
+#       print(i)
+
+#while
+
+while 0<50:
+    
