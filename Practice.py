5668e99e49c06e6118bc90231cb59c1a6044ee40
Steve <stevetsaoch@gmail.com>
2020-09-24 15:38:52 +0800

    20200924

--- Practice.py
+++ Practice.py
@@ -1,1 +1,16 @@
-"123"
+#practice for loop
+
+for item in [1,2,3,4,5]:
+    print(item)
+
+user = {
+    'name' :'Steve',
+    'age' : 10,
+    'Height' : 100
+}
+
+for key, value in user.items():
+    print(key, value)
+
+for item in user.values():
+    print(item)
