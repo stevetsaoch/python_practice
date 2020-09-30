67ac7c00c5ad5967b47cbda11579897165de03ac
Steve <stevetsaoch@gmail.com>
2020-09-30 15:15:27 +0800

    course of test

---
+++ test.py
@@ -0,0 +1,28 @@
+import unittest
+import main
+
+
+class TestMain(unittest.TestCase):
+    def test_do_stuff(self):
+        test_param = 10
+        result = main.do_stuff(test_param)
+        self.assertEqual(result, 15)
+
+    def test_do_stuff2(self):
+        test_param = 'sadfa'
+        result = main.do_stuff(test_param)
+        self.assertTrue(isinstance(result, ValueError))
+
+    def test_to_do_stuff3(self):
+        test_param = None
+        result = main.do_stuff(test_param)
+        self.assertEqual(result, 'please enter number')
+
+    def test_to_do_stuff4(self):
+        test_param = ''
+        result = main.do_stuff(test_param)
+        self.assertEqual(result, 'please enter number')
+
+
+if __name__ == '__main__':
+    unittest.main()
